ffce94bc89de0a4f22855ae96d06f744ffc2df95
varunchandwani <varunchandwani@gmail.com>
2021-03-16 20:29:47 +1100

    Missing prop passed

--- src/components/Post/Post.test.tsx
+++ src/components/Post/Post.test.tsx
@@ -34,6 +34,7 @@ const store = mockStore({
         body:
           'quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto',
         author: 'Varun',
+        keyVal: 'key_1',
       },
     ],
   },
